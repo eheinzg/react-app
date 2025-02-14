05476cec6e23a90eb460f922b11587a4949a76ab
Erwin Goscin <eheinzg@gmail.com>
2025-02-14 00:15:48 -0500

    commmit5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   let heroData = [
@@ -14,6 +15,7 @@ const App = () => {
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
     </div>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
-import ' ./Navbar.css'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='nav'>
+        <div className="nav-logo">EV-olution</div>
+        <ul className='nav-menu'>
+          <li>Home</li>
+          <li>Explore</li>
+          <li>About</li>
+          <li className='nav-contact'>Contact</li>
+        </ul>
     </div>
   )
 }
